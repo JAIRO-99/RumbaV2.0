387708f4ee806de9289f5d949629e82b9d5e9f80
Jairo <jairo.lcelis1999@gmail.com>
2024-09-03 09:32:16 -0500

    Initial Commit

---
+++ RumbaV2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RumbaV2.0
+//
+//  Created by Jairo Laurente Celis on 3/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RumbaV2.0/RumbaV2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  RumbaV2_0App.swift
+//  RumbaV2.0
+//
+//  Created by Jairo Laurente Celis on 3/09/24.
+//
+
+import SwiftUI
+
+@main
+struct RumbaV2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
